a33f35d75f950f13f02f09bdbf14da1558d4aa70
Jololio <jala9132@740s-pl-120.local>
2016-09-14 12:57:43 -0600

    Added statements about monster

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,11 +1,29 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController 
 {
+	private MarshmallowMonster firstMonster;
 	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("Crusher of Dreams", 2, 2, 0, 3, true);
+		
+	}
 	
 	public void start()
 	{
-		
+		System.out.println("We made monsters today!");
+		System.out.println("Here is mine " + firstMonster);
 	}
+	
+	/*
+	 * monster name is Crusher of Dreams
+	 * has 2 antennai
+	 * has 2 legs
+	 * has no nose
+	 * has 3 eyes
+	 * 1 bellybutton
+	 */
 }
---
+++ src/monster/model/MarshmallowMonster.java
@@ -0,0 +1,33 @@
+package monster.model;
+
+public class MarshmallowMonster
+{
+	private String name;
+	private double antennaCount;
+	private int eyeCount;
+	private int legCount;
+	private int noseCount;
+	private boolean hasBellyButton;
+	
+	public MarshmallowMonster()
+	{
+		this.name = "no name";
+		this.antennaCount = -228282.45;
+		this.eyeCount = -12;
+		this.legCount = -2231;
+		this.noseCount = -2;
+		this.hasBellyButton = false;
+	}
+	
+	public MarshmallowMonster(String name, double antennaCount, int eyeCount, int legCount, int noseCount, boolean hasBellyButton)
+	
+	{
+	
+		this.name = name;
+		this.antennaCount = antennaCount;
+		this.eyeCount = eyeCount;
+		this.legCount = legCount;
+		this.noseCount = noseCount;
+		this.hasBellyButton = hasBellyButton;
+	}
+}
